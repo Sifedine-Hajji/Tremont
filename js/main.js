876b87baef98453d992fe41cb184ef4d03451f94
unknown <Sifedinehajji01@gmail.com>
2021-12-16 15:33:31 +0100

    adding tremont files

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Animation lors du scroll disparition de la nav et +
+const ScrollAnimation = () => {
+    let posScrollPre = window.pageYOffset;
+    window.onscroll = function () {
+        let posScrollNow = window.pageYOffset;
+
+        if (posScrollPre > posScrollNow) {
+            document.querySelector("nav").style.top = 0;
+        } else {
+            document.querySelector("nav").style.top = "-100px";
+            document.querySelector(".title-container").style.opacity = '0';
+        }
+        if (posScrollNow == 0) {
+            document.querySelector(".title-container").style.opacity = '1';
+        }
+
+        posScrollPre = posScrollNow;
+    }
+}
+
+// FADE IN AND OUT ANIMATION
+const FadeOut = () => {
+    window.onload = () => {
+        const transition = document.querySelector('.transition')
+        const navLinks = document.querySelectorAll('nav a')
+
+        setTimeout(() => {
+            transition.classList.remove('active');
+        }, 300);
+
+        for (let i = 0; i < navLinks.length; i++) {
+            const navLink = navLinks[i];
+
+            navLink.addEventListener('click', (e) => {
+                e.preventDefault();
+                let target = e.target.href;
+                transition.classList.add('active');
+
+                setTimeout(() => {
+                    window.location.href = target;
+                }, 500);
+            })
+        }
+    }
+};
+
+
+
+
+const app = () => {
+    ScrollAnimation();
+    FadeOut();
+}
+
+app();
